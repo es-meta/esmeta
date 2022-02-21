b70c487458c4074535d8f01b4ec5b5ddd3b8559e
Kanguk Lee <p51lee@kaist.ac.kr>
2022-02-21 14:07:24 +0900

    Generate pdf files from dot files

--- src/main/scala/esmeta/phase/BuildCFG.scala
+++ src/main/scala/esmeta/phase/BuildCFG.scala
@@ -16,18 +16,37 @@ case object BuildCFG extends Phase[Spec, Spec] {
     globalConfig: GlobalConfig,
     config: Config,
   ): Spec = {
+    def norm(name: String) = name
+      .replace("/", "")
+      .replace(" ", "")
+      .replace("<", "")
+      .replace(">", "")
+      .replace("`", "")
     val cfg = spec.program.cfg
 
     if (config.dot) {
       mkdir(CFG_LOG_DIR)
       // dump dot format
       cfg.funcs.foreach(f => {
-        val name = s"${CFG_LOG_DIR}/${f.ir.name.replace("/", "")}"
+        val name =
+          s"${CFG_LOG_DIR}/${norm(f.ir.name)}"
         dumpFile(f.toDot, s"$name.dot")
       })
-
+      // check wheter dot is available
       if (config.pdf) {
-        ??? // dump pdf format
+        if (isNormalExit("dot -V")) {
+          cfg.funcs.foreach(f => {
+            val name =
+              s"${CFG_LOG_DIR}/${norm(f.ir.name)}"
+            try executeCmd(s"dot -Tpdf $name.dot -o $name.pdf")
+            catch {
+              case ex: Exception =>
+                println(
+                  s"[ERROR] $name: exception occured while converting to pdf",
+                )
+            }
+          })
+        } else println("Dot is not installed!")
       }
     }
 
