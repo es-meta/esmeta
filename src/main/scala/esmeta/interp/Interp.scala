feb901be10b613d15e97452ade9c11e0cdc3278d
h2oche <h2oche22@gmail.com>
2022-03-18 12:59:40 +0900

    Handled error in StringToBigInt (#61)

--- src/main/scala/esmeta/interp/Interp.scala
+++ src/main/scala/esmeta/interp/Interp.scala
@@ -252,13 +252,17 @@ class Interp(
     case EConvert(cop, expr) =>
       import COp.*
       (interp(expr).escaped, cop) match {
-        case (Math(n), ToNumber)   => Number(n.toDouble)
-        case (Math(n), ToBigInt)   => BigInt(n.toBigInt)
-        case (Str(s), ToNumber)    => Number(ESValueParser.str2Number(s))
-        case (Str(s), ToBigInt)    => ESValueParser.str2bigint(s)
-        case (POS_INF, ToMath)     => POS_INF
-        case (NEG_INF, ToMath)     => NEG_INF
-        case (Number(d), ToMath)   => Math(BigDecimal.exact(d))
+        case (Math(n), ToNumber) => Number(n.toDouble)
+        case (Math(n), ToBigInt) => BigInt(n.toBigInt)
+        case (Str(s), ToNumber)  => Number(ESValueParser.str2Number(s))
+        case (Str(s), ToBigInt) =>
+          ESValueParser.str2bigint(s) match
+            case b: BigInt            => b
+            case Number(d) if d.isNaN => Undef
+            case v                    => throw InvalidConversion(cop, expr, v)
+        case (POS_INF, ToMath) => POS_INF
+        case (NEG_INF, ToMath) => NEG_INF
+        case (Number(d), ToMath) => Math(BigDecimal.exact(d))
         case (CodeUnit(c), ToMath) => Math(BigDecimal.exact(c.toInt))
         case (BigInt(n), ToMath)   => Math(BigDecimal.exact(n))
         case (Number(d), ToStr(radixOpt)) =>
