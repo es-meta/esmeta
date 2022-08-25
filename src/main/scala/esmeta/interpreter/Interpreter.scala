499ca944b6230be6b8b9f8f7d8837795299cdf5b
antony-jeong <antony@kaist.ac.kr>
2022-08-25 15:39:35 +0900

    Add manual rules for BigInt::toString and INTRINSICS.BigInt.prototype.toString

--- src/main/scala/esmeta/interpreter/Interpreter.scala
+++ src/main/scala/esmeta/interpreter/Interpreter.scala
@@ -269,10 +269,12 @@ class Interpreter(
         case (Number(d), ToMath)          => Math(BigDecimal.exact(d))
         case (CodeUnit(c), ToMath)        => Math(BigDecimal.exact(c.toInt))
         case (BigInt(n), ToMath)          => Math(BigDecimal.exact(n))
-        case (BigInt(n), ToStr(None))     => Str(n.toString())
         case (Number(d), ToStr(radixOpt)) =>
           val radix = radixOpt.fold(10)(e => eval(e).asInt)
           Str(toStringHelper(d, radix))
+        case (BigInt(n), ToStr(radixOpt)) =>
+          val radix = radixOpt.fold(10)(e => eval(e).asInt)
+          Str(n.toString(radix))
         case (BigInt(n), ToBigInt) => BigInt(n)
         case (Math(n), ToMath)     => Math(n)
         case (Number(d), ToNumber) => Number(d)
