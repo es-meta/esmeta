686ccec3144b27c74eccbfce400c71c18cb49e0b
Kanguk Lee <p51lee@kaist.ac.kr>
2022-08-31 17:35:36 +0900

    Implement some helper ast classes

--- src/main/scala/esmeta/es/util/mutator/RandomSynth.scala
+++ src/main/scala/esmeta/es/util/mutator/RandomSynth.scala
@@ -9,6 +9,8 @@ import esmeta.util.*
 
 /** ast synthesizer for random mutation */
 class RandomSynth(grammar: Grammar) {
+  var fromNonterminal: String = "AssignmentExpression"
+  var toNonterminal: String = "RelationalExpression"
 
   /** a mapping from names to productions */
   lazy val nameMap: Map[String, Production] = grammar.nameMap
@@ -31,39 +33,53 @@ class RandomSynth(grammar: Grammar) {
     revMap
   }
 
-//  type GrammarStack = List[String]
-//  type GrammarRoute = List[List[String]]
-//  type GrammarTouch = Set[String]
   case class GrammarState(
     stack: List[String],
     routes: List[List[String]],
     touched: Set[String],
   )
 
-  val grammarMonad: StateMonad[GrammarState] =
-    StateMonad[GrammarState]()
+//  val grammarMonad: StateMonad[GrammarState] =
+//    StateMonad[GrammarState]()
+
+  def searchRoute(state: GrammarState): GrammarState = {
+    reversedMap(state.stack.head).foldLeft(state) {
+      case (GrammarState(stack, routes, touched), producer) =>
+        if (producer == fromNonterminal)
+          GrammarState(
+            stack,
+            (producer :: stack) :: routes,
+            touched + producer,
+          )
+        else if (touched.contains(producer))
+          GrammarState(stack, routes, touched)
+        else
+          searchRoute(
+            GrammarState(producer :: stack, routes, touched + producer),
+          ).copy(stack = stack)
+    }
+  }
+  def getRouteParam(route: List[String]): List[String] = {
+    route match {
+      case head :: next => {
+        val prod = nameMap(head)
+        prod.rhsList
+
+        ???
+      }
+      case Nil => Nil
+    }
+  }
 
   def synthesize(from: String, to: String): Ast => Ast = {
+    fromNonterminal = from
+    toNonterminal = to
 
-    def searchRoute(state: GrammarState): GrammarState = {
-      reversedMap(state.stack.head).foldLeft(state) {
-        case (GrammarState(stack, routes, touched), producer) =>
-          if (producer == from)
-            GrammarState(
-              stack,
-              (producer :: stack) :: routes,
-              touched + producer,
-            )
-          else if (touched.contains(producer))
-            GrammarState(stack, routes, touched)
-          else
-            searchRoute(
-              GrammarState(producer :: stack, routes, touched + producer),
-            ).copy(stack = stack)
-      }
-    }
-    val finalState = searchRoute(GrammarState(List(to), List.empty, Set.empty))
-    finalState.routes.foreach(_.foreach(println(_)))
+    val routes = searchRoute(
+      GrammarState(List(toNonterminal), List.empty, Set.empty),
+    ).routes
+
+    routes.foreach(_.foreach(println(_)))
     x => x
   }
 }
