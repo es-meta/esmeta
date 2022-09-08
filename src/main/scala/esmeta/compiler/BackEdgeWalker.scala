708c365624383837400464c8146eec64b5eceb70
Jae Hyun Lee <99jaehyunlee@gmail.com>
2022-09-08 14:45:17 +0900

    Added backward edge of Expression in compiler

--- src/main/scala/esmeta/compiler/BackEdgeWalker.scala
+++ src/main/scala/esmeta/compiler/BackEdgeWalker.scala
@@ -1,6 +1,6 @@
 package esmeta.compiler
 
-import esmeta.ir.Inst
+import esmeta.ir.{Inst, Expr}
 import esmeta.ir.util.Walker
 
 /** walker for adjusting backward edge from ir to lang */
@@ -11,4 +11,8 @@ case class BackEdgeWalker(
   override def walk(i: Inst) =
     if (force || i.langOpt.isEmpty) i.setLangOpt(fb.langs.headOption)
     super.walk(i)
+
+  override def walk(e: Expr) =
+    if (force || e.langOpt.isEmpty) e.setLangOpt(fb.langs.headOption)
+    super.walk(e)
 }
