7e6d09da020927412fbbb991fe19915594ae43b4
suhyeonPark <0111suhyeon@kaist.ac.kr>
2022-09-29 20:59:46 +0900

    valueTy contains value function for address value

--- src/main/scala/esmeta/ty/ValueTy.scala
+++ src/main/scala/esmeta/ty/ValueTy.scala
@@ -109,11 +109,43 @@ case class ValueTy(
 
   /** value containment check */
   def contains(value: Value, state: State): Boolean = value match
-    case Comp(ty, value, target)         => isCompletion
-    case NamedAddr(name)                 => ???
-    case DynamicAddr(long)               => ???
-    case Clo(func, captured)             => ???
-    case Cont(func, captured, callStack) => ??? // cont.isBottom
+    case Comp(ty, value, target) => isCompletion
+    // address 굳이 두개로 나누지 않아도 됨.
+    case a: Addr =>
+      state.heap(a) match {
+        case m @ MapObj(ty, props, _) =>
+          (names contains ty) | m.keys.foldLeft(true)({
+            case (t, key) => (
+              t && (record.map.get(key.asStr) match
+                case None => false
+                case Some(v) =>
+                  v match
+                    case None    => true
+                    case Some(v) => v.contains(props.get(key).get.value, state)
+              )
+            )
+          }) // MapObj 는 purevalue -> props 이고 record type은 string -> valueTy 인데 key.asStr 으로 넘길 수 있는지..
+        // or named record 즉 (mapobj의 ty 가 names 안에 존재한다면) 면 name 안에 들어갈 시 그냥 바로 true retur면
+        // 아닐경우 mapobj 의 pairs 를 가져와서 record의 keyset의 subset인지 확인하고 각 mapobj 의 value가 record에서 지정된 type 안에 들어가는지 체크
+        // record에서 value type은 none일 경우 bottom Some(none)일 경우 Top
+        // 각 value in mapobj pair 가 record의 valuetype에 들어가는 지 체크해주어야함.
+        case ListObj(values) =>
+          list.elem match
+            case None => false
+            case Some(ty) =>
+              values.foldLeft(true)({
+                case (t, v) => t && (ty.contains(v, state))
+              })
+
+        // ListObj(p) 에서 p에 각 element 마다 for each 로 check 다 해주어야함
+        // list 로 가져온거중에 elem 이 none 일 경우 아무것도 올 수 없다.
+        // list 로 가져온거중에 elem 이 Some(type)인 경우 for each 로 ListObj(p)에 p를 iterate 해야함
+        case SymbolObj(_) => symbol
+        case YetObj(_, _) => true
+      }
+    case Clo(func, captured)             => clo contains func.irFunc.name
+    case Cont(func, captured, callStack) => cont contains func.id
+    // why does clo return BSet[String] and why does cont return BSet[Int]
     case AstValue(ast) =>
       astValue match
         case AstTopTy       => true
@@ -122,7 +154,7 @@ case class ValueTy(
     case Math(n)                   => math contains n
     case Const(name)               => const contains name
     case CodeUnit(c)               => codeUnit
-    case Number(n)                 => number contains n
+    case num @ Number(n)           => number contains num
     case BigInt(n)                 => bigInt
     case Str(s)                    => str contains s
     case Bool(b)                   => bool contains b
