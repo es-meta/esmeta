65163750fe774714ad89048d2f0d6d52666c06f1
Jihyeok Park <jhpjhp0223@gmail.com>
2022-11-25 15:22:38 +0900

    Supported -test262dir global option (#110)

--- src/main/scala/esmeta/package.scala
+++ src/main/scala/esmeta/package.scala
@@ -48,14 +48,16 @@ val PACKAGE_NAME = "esmeta"
 /** tests directory */
 val IR_TEST_DIR = s"$TEST_DIR/ir"
 val ES_TEST_DIR = s"$TEST_DIR/es"
-val TEST262_DIR = s"$TEST_DIR/test262"
-val TEST262_TEST_DIR = s"$TEST262_DIR/test"
 
 /** error stack trace display mode */
 var ERROR_MODE = false
 
 /** exit status return mode */
 var STATUS_MODE = false
+
+/** test262 directories */
+var TEST262_DIR = s"$TEST_DIR/test262"
+def TEST262_TEST_DIR = s"$TEST262_DIR/test"
 
 // -----------------------------------------------------------------------------
 // Mutable Global Options
