b57ede1200b3ee074c7b4ccab0ca182829c86e05
jhnaldo <jhpjhp0223@gmail.com>
2022-12-01 18:21:55 +0900

    Fixed bugs in toStringHelper

--- src/main/scala/esmeta/state/package.scala
+++ src/main/scala/esmeta/state/package.scala
@@ -3,7 +3,7 @@ package esmeta.state
 import esmeta.state.util.*
 import esmeta.util.BaseUtils.*
 import esmeta.ir.Global
-import java.math.MathContext.UNLIMITED
+import java.math.MathContext.{UNLIMITED, DECIMAL128}
 
 /** IR state elements */
 trait StateElem {
@@ -64,58 +64,99 @@ val POS_INF = Number(Double.PositiveInfinity)
 val NEG_INF = Number(Double.NegativeInfinity)
 
 /** conversion number to string */
-def toStringHelper(m: Double, radix: Int = 10): String = {
+def toStringHelper(x: Double, radix: Int = 10): String = {
   // get sign
   def getSign(n: Int): Char = if (n - 1 > 0) '+' else '-'
 
   // get string of number
-  def getStr(number: Long, radix: Int): String =
+  def getStr(number: scala.math.BigInt, radix: Int): String =
     var str = ""
-    var sLong = number
-    while (sLong > 0) { str += getRadixString(sLong % radix); sLong /= radix }
+    var s = number
+    while (s > 0) { str += getRadixString(s % radix); s /= radix }
     str.reverse
 
   // get radix string of number
-  def getRadixString(d: Long): String =
+  def getRadixString(d: scala.math.BigInt): String =
     if (d < 10) d.toString else ('a' + (d - 10)).toChar.toString
 
-  def INT(n: Int): BigDecimal = BigDecimal(n, UNLIMITED)
-  def DOUBLE(n: Double): BigDecimal = BigDecimal(n, UNLIMITED)
-
-  if (m.isNaN) "NaN"
-  else if (m == 0) "0"
-  else if (m < 0) "-" + toStringHelper(-m, radix)
-  else if (m.isPosInfinity) "Infinity"
+  // 1. If _x_ is *NaN*, return the String *"NaN"*.
+  if (x.isNaN) "NaN"
+  // 2. If _x_ is *+0*<sub>𝔽</sub> or *-0*<sub>𝔽</sub>, return the String
+  //    *"0"*.
+  else if (x == 0) "0"
+  // 3. If _x_ < *-0*<sub>𝔽</sub>, return the string-concatenation of *"-"* and
+  //    toStringHelper(-_x_).
+  else if (x < 0) "-" + toStringHelper(-x, radix)
+  // 4. If _x_ is *+∞*<sub>𝔽</sub>, return the String *"Infinity"*.
+  else if (x.isPosInfinity) "Infinity"
   else {
-    var s = DOUBLE(m)
-    var n = 0
-    while (s % radix == INT(0) || s % 1 != INT(0)) {
-      if (s % radix == INT(0)) { s /= radix; n += 1 }
-      else { s *= radix; n -= 1 }
-    }
-    while (
-      (((s - (s % radix)) / radix) * INT(radix).pow(n + 1)).toDouble == m
-    ) {
-      s = (s - (s % radix)) / radix
-      n = n + 1
-    }
-    var sLong = s.toLong
-    var k = 0
-    while (s >= INT(1)) { s /= radix; k += 1 }
-    n += k
+    // 5. Otherwise, let _n_, _k_, and _s_ be integers such that _k_ >= 1,
+    //    _radix_<sup>_k_ - 1</sup> <= _s_ < _radix_<sup>_k_</sup>, 𝔽(_s_ *
+    //    _radix_<sup>_n_ - _k_</sup>) is _x_, and _k_ is as small as possible.
+    val (n: Int, k: Int, s: scala.math.BigInt) =
+      var S = BigDecimal(x, UNLIMITED)
+      var N = 0
+      while (S % radix == 0) { S /= radix; N += 1 }
+      while (S % 1 != 0) { S *= radix; N -= 1 }
+      var RK = BigDecimal(radix, UNLIMITED)
+      var K = 1
+      while (S >= RK) { RK *= radix; K += 1 }
+      (N + K, K, S.toBigInt)
+
     if (k <= n && n <= 21) {
-      getStr(sLong, radix) + ("0" * (n - k))
+      // * the code units of the _k_ digits of the decimal representation of
+      //   _s_ (in order, with no leading zeroes)
+      getStr(s, radix) +
+      // * _n_ - _k_ occurrences of the code unit 0x0030 (DIGIT ZERO)
+      "0" * (n - k)
     } else if (0 < n && n <= 21) {
-      val str = getStr(sLong, radix)
-      str.substring(0, n) + '.' + str.substring(n)
+      val str = getStr(s, radix)
+      // * the code units of the most significant _n_ digits of the decimal
+      //   representation of _s_
+      str.substring(0, n) +
+      // * the code unit 0x002E (FULL STOP)
+      '.' +
+      // * the code units of the remaining _k_ - _n_ digits of the decimal
+      //   representation of _s_
+      str.substring(n)
     } else if (-6 < n && n <= 0) {
-      "0." + ("0" * (-n)) + getStr(sLong, radix)
+      // * the code unit 0x0030 (DIGIT ZERO)
+      "0" +
+      // * the code unit 0x002E (FULL STOP)
+      "." +
+      // * -_n_ occurrences of the code unit 0x0030 (DIGIT ZERO)
+      "0" * -n +
+      // * the code units of the _k_ digits of the decimal representation of _s_
+      getStr(s, radix)
     } else if (k == 1) {
-      getStr(sLong, radix) + "e" + getSign(n) + math.abs(n - 1).toString
+      // * the code unit of the single digit of _s_
+      getStr(s, radix) +
+      // * the code unit 0x0065 (LATIN SMALL LETTER E)
+      "e" +
+      // * the code unit 0x002B (PLUS SIGN) or the code unit 0x002D
+      //   (HYPHEN-MINUS) according to whether _n_ - 1 is positive or negative
+      getSign(n) +
+      // * the code units of the decimal representation of the integer abs(_n_
+      //   - 1) (with no leading zeroes)
+      math.abs(n - 1)
     } else {
-      val str = getStr(sLong, radix)
-      str.substring(0, 1) + '.' + str
-        .substring(1) + 'e' + getSign(n) + math.abs(n - 1).toString
+      val str = getStr(s, radix)
+      // * the code units of the most significant digit of the decimal
+      //   representation of _s_
+      str.substring(0, 1) +
+      // * the code unit 0x002E (FULL STOP)
+      '.' +
+      // * the code units of the remaining _k_ - 1 digits of the decimal
+      //   representation of _s_
+      str.substring(1) +
+      // * the code unit 0x0065 (LATIN SMALL LETTER E)
+      'e' +
+      // * the code unit 0x002B (PLUS SIGN) or the code unit 0x002D
+      //   (HYPHEN-MINUS) according to whether _n_ - 1 is positive or negative
+      getSign(n) +
+      // * the code units of the decimal representation of the integer abs(_n_
+      //   - 1) (with no leading zeroes)
+      math.abs(n - 1)
     }
   }
 }
