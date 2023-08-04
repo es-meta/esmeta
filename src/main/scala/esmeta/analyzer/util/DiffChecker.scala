362dd2eaa5f54af43e830506f55836a8f18e6568
kimjg1119 <kimjg1199@naver.com>
2023-08-05 06:10:48 +0900

    Add logging Differences occur
    Now when performing range-tycheck operation, records specific typeerrors which appears or disappears.

--- src/main/scala/esmeta/analyzer/util/DiffChecker.scala
+++ src/main/scala/esmeta/analyzer/util/DiffChecker.scala
@@ -9,13 +9,37 @@ object DiffChecker {
   def diffBetweenTwo(
     leftPath: String,
     rightPath: String,
+    record: Boolean = true,
   ): (List[String], List[String]) = {
     val left = optional(readFile(leftPath).split(LINE_SEP).toSet)
       .getOrElse(throw ESMetaError(s"File not found: $leftPath"))
     val right = optional(readFile(rightPath).split(LINE_SEP).toSet)
       .getOrElse(throw ESMetaError(s"File not found: $rightPath"))
 
+    val ldiff = left.diff(right).toList.sorted
+    val rdiff = right.diff(left).toList.sorted
+
+    if (record) {
+      val delPath = s"$rightPath/removed"
+      val addPath = s"$rightPath/added"
+      if (ldiff.nonEmpty)
+        dumpFile(
+          name = "deleted type errors",
+          data = ldiff.mkString(LINE_SEP),
+          filename = delPath,
+          silent = true,
+        )
+      if (rdiff.nonEmpty)
+        dumpFile(
+          name = "added type errors",
+          data = rdiff.mkString(LINE_SEP),
+          filename = addPath,
+          silent = true,
+        )
+    }
+
+    (ldiff, rdiff)
+
     // deleted, added
-    (left.diff(right).toList.sorted, right.diff(left).toList.sorted)
   }
 }
