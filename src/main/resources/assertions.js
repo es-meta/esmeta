486af4a0ded34484e9ae6917781263637df2da85
Yongwook Choi <flow@hrmm.xyz>
2024-04-04 00:42:38 +0900

    update assertions.js - this should be in injector patch

--- src/main/resources/assertions.js
+++ src/main/resources/assertions.js
@@ -1,77 +1,67 @@
-"use strict";
-
 // hidden constructors
-var AsyncArrowFunction = Object.getPrototypeOf(async () => {}).constructor;
-var AsyncFunction = Object.getPrototypeOf(async function () {}).constructor;
-var AsyncGeneratorFunction = Object.getPrototypeOf(
-  async function* () {}
+let AsyncArrowFunction = Object.getPrototypeOf(async () => {}).constructor;
+let AsyncFunction = Object.getPrototypeOf(async function () {}).constructor;
+let AsyncGeneratorFunction = Object.getPrototypeOf(
+  async function* () {},
 ).constructor;
-var GeneratorFunction = Object.getPrototypeOf(function* () {}).constructor;
+let GeneratorFunction = Object.getPrototypeOf(function* () {}).constructor;
 
 // logging errors
-var $error = (globalThis.console && globalThis.console.log) || globalThis.print;
-
-// algo map
-var $algo = new Map();
+let $error = (globalThis.console && globalThis.console.log) || globalThis.print;
 
 // conversion to string
-function $toString(value) {
+let $toString = (value) => {
   if (value === 0 && 1 / value === -Infinity) return "«-0»";
   if (value instanceof Error) return "a " + value.constructor.name;
+  if (value === AsyncFunction.prototype) return "the async function prototype";
+  if (value === AsyncGeneratorFunction.prototype) {
+    return "the async generator function prototype";
+  }
+  if (value === GeneratorFunction.prototype) {
+    return "the generator function prototype";
+  }
+  if (value === AsyncArrowFunction.prototype) {
+    return "the async arrow function prototype";
+  }
+  if (value === Object.prototype) return "the object prototype";
   if (typeof value === "string") return '"' + value + '"';
+  if (typeof value === "function") return "[object Function]";
+  if (typeof value === "object") return "[object Object]";
   return String(value);
-}
+};
 
-function $isSameValue(x, y) {
+// wrapper of Object.getPrototpyeOf
+let $Object_getPrototypeOf = (o) => {
+  if (o === null || o === undefined) {
+    return undefined;
+  }
+  return Object.getPrototypeOf(o);
+};
+
+// wrapper of Reflect.ownKeys
+let $Reflect_ownKeys = (o) => {
+  if (o === null || o === undefined) {
+    return undefined;
+  }
+  return Reflect.ownKeys(o);
+};
+
+let $isSameValue = (x, y) => {
   if (x === y) return x !== 0 || 1 / x === 1 / y;
   return x !== x && y !== y;
-}
+};
 
 // assertion
-function $assert(mustBeTrue) {
+let $assert = (mustBeTrue) => {
   if (mustBeTrue === true) return;
   $error("Expected true but got " + $toString(mustBeTrue));
-}
-
-// assertion for comparing two thrown values
-$assert.sameThrows = function (thrown, expected) {
-  var thrownStr = $toString(thrown);
-  if (typeof expected !== "function") {
-    if (thrown !== expected)
-      $error("Expected " + $toString(expected) + " but got " + thrownStr);
-  } else if (!(thrown instanceof expected)) {
-    $error("Expected a " + expected.name + " but got " + thrownStr);
-  }
-};
-
-// assertion for thrown values that were expected to be thrown but not thrown
-$assert.shouldveThrown = function (expected) {
-  $error(
-    "Expected a " +
-      expected.name +
-      " to be thrown but no exception was thrown at all"
-  );
-};
-
-// assertion for no exception
-$assert.notThrows = function (func) {
-  if (typeof func !== "function") {
-    $error("$assert.notThrows requires a function.");
-    return;
-  }
-  try {
-    func();
-  } catch (thrown) {
-    $error("Expected no exception but " + $toString(thrown) + " is thrown.");
-    return;
-  }
 };
 
 // assertion for same values
 $assert.sameValue = function (actual, expected) {
   if ($isSameValue(actual, expected)) return;
   $error(
-    "Expected " + $toString(expected) + " but got " + $toString(actual) + "."
+    "Expected " + $toString(expected) + " but got " + $toString(actual) + ".",
   );
 };
 
@@ -83,7 +73,7 @@ $assert.notSameValue = function (actual, unexpected) {
       $toString(unexpected) +
       " but got " +
       $toString(actual) +
-      "."
+      ".",
   );
 };
 
@@ -92,12 +82,14 @@ $assert.isCallable = function (f) {
   return typeof f === "function";
 };
 $assert.callable = function (f) {
-  if (!$assert.isCallable(f))
+  if (!$assert.isCallable(f)) {
     $error("Expected " + $toString(f) + " has [[Call]] but does not.");
+  }
 };
 $assert.notCallable = function (f) {
-  if ($assert.isCallable(f))
+  if ($assert.isCallable(f)) {
     $error("Expected " + $toString(f) + " does not have [[Call]] but does.");
+  }
 };
 
 // assertion for [[Construct]]
@@ -110,44 +102,74 @@ $assert.isConstructable = function (f) {
   }
 };
 $assert.constructable = function (f) {
-  if (!$assert.isConstructable(f))
+  if (!$assert.isConstructable(f)) {
     $error("Expected " + $toString(f) + " has [[Construct]] but does not.");
+  }
 };
 $assert.notConstructable = function (f) {
-  if ($assert.isConstructable(f))
+  if ($assert.isConstructable(f)) {
     $error(
-      "Expected " + $toString(f) + " does not have [[Construct]] but does."
+      "Expected " + $toString(f) + " does not have [[Construct]] but does.",
     );
+  }
 };
 
 // assertion to compare arrays
-function $compareArray(a, b) {
+let $compareArray = (a, b) => {
   if (b.length !== a.length) return false;
-  for (var i = 0; i < a.length; i++) {
-    if (!$isSameValue(a[i], b[i])) return false;
+
+  let sortedA = a.slice();
+  let sortedB = b.slice();
+  /* Allow change in order */
+  try {
+    sortedA.sort();
+    sortedB.sort();
+  } catch {
+    sortedA = a;
+    sortedB = b;
+  }
+
+  for (var i = 0; i < sortedA.length; i++) {
+    if (!$isSameValue(sortedA[i], sortedB[i])) return false;
   }
   return true;
-}
+};
 
 $assert.compareArray = function (actual, expected, obj) {
   function format(array) {
     return "[" + array.map($toString).join(", ") + "]";
   }
   function getObjDesc(obj) {
-    var algo = $algo.get(obj) || "Nothing";
     var ty = Object.prototype.toString.call(obj);
-    ty = ty.substring("[object ".length, ty.length - "]".length);
-    return `${algo} for ${ty}`;
+    return ty.substring("[object ".length, ty.length - "]".length);
   }
+
+  if (!Array.isArray(actual)) {
+    $error(
+      "$assert.compareArray requires an array as the first argument but " +
+        $toString(actual) +
+        " given.",
+    );
+    return;
+  }
+  if (!Array.isArray(expected)) {
+    $error(
+      "$assert.compareArray requires an array as the second argument but " +
+        $toString(expected) +
+        " given.",
+    );
+    return;
+  }
+
   if ($compareArray(actual, expected)) return;
   $error(
     "Expected " +
       format(expected) +
       " but got " +
       format(actual) +
-      " in " +
+      " for " +
       getObjDesc(obj) +
-      "."
+      ".",
   );
 };
 
@@ -162,31 +184,41 @@ $assert.compareIterator = function (iter, validators) {
         i +
         " values(s). Instead iterator only produced " +
         (i - 1) +
-        " value(s)."
+        " value(s).",
     );
     validators[i](result.value);
   }
   result = iter.next();
   $error(
     result.done,
-    "Expected only " + i + " values(s). Instead iterator produced more."
+    "Expected only " + i + " values(s). Instead iterator produced more.",
   );
   $assert.sameValue(
     result.value,
     undefined,
-    "Expected value of `undefined` when iterator completes."
+    "Expected value of `undefined` when iterator completes.",
   );
 };
 
 // verify properties
-function $verifyProperty(obj, prop, desc) {
+let $verifyProperty = (obj, prop, desc) => {
+  // check object
+  if (obj === undefined || obj === null) {
+    $error(
+      "$verifyProperty requires an object but " +
+        $toString(obj) +
+        " given.",
+    );
+    return;
+  }
+
   // check property type
   var propType = typeof prop;
   if (propType !== "string" && propType !== "symbol") {
     $error(
       "$verifyProperty requires a string or symbol property but " +
         $toString(prop) +
-        " given."
+        " given.",
     );
     return;
   }
@@ -200,7 +232,12 @@ function $verifyProperty(obj, prop, desc) {
   }
 
   var hasOwnProperty = Object.prototype.hasOwnProperty;
-  $assert(hasOwnProperty.call(obj, prop));
+  if (!hasOwnProperty.call(obj, prop)) {
+    $error(
+      $toString(obj) + " does not have own property named " + $toString(prop),
+    );
+    return;
+  }
   $assert.notSameValue(desc, null);
   $assert.sameValue(typeof desc, "object");
 
@@ -209,15 +246,16 @@ function $verifyProperty(obj, prop, desc) {
       if (!hasOwnProperty.call(desc, name)) return;
       if ($isSameValue(desc[name], originalDesc[name])) return;
       var message;
-      if (name === "value")
-        message =
-          "descriptor value should be " +
+      if (name === "value") {
+        if ($toString(prop) == "name") return; /* Allow different name */
+        message = "descriptor value of " + $toString(prop) + " should be " +
           $toString(desc.value) +
           " but " +
           $toString(originalDesc.value);
-      else
-        message =
-          "descriptor should " + (desc[name] ? "" : "not ") + "be " + name;
+      } else {
+        message = "descriptor should " + (desc[name] ? "" : "not ") + "be " +
+          name;
+      }
       $error(message);
     } catch (e) {}
   }
@@ -225,19 +263,33 @@ function $verifyProperty(obj, prop, desc) {
   check("writable");
   check("enumerable");
   check("configurable");
-}
+};
 
-// delay checking assertions
-function $delay(f) {
-  var DELAY = 100;
+// delay checking assertions in JS runtime environment
+// (supported: Node, QuickJs)
+// or wait for 10 microtasks in JS Engine
+// (might be broken if there are more than 10 awaits in program, etc.)
+let $delay = (f) => {
   var setTimeout = globalThis.setTimeout;
   import("os")
     .then((os) => {
       // qjs
-      if (!setTimeout) setTimeout = os?.setTimeout;
+      setTimeout ??= os?.setTimeout;
     })
     .catch(() => {})
-    .finally(() => {
-      setTimeout(f, DELAY);
+    .then(() => {
+      let p;
+      if (setTimeout) {
+        p = new Promise((resolve) => setTimeout(resolve, 0));
+      } else {
+        p = Promise.resolve();
+        // delay 10 times
+        for (var i = 0; i < 10; i++) {
+          p = p.then(() => {});
+        }
+      }
+      p.then(f).catch(() =>
+        $error("An exception occured while checking assertions")
+      );
     });
-}
+};
