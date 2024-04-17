0021edaf621c0500f45cbae868d034795a19654b
Yongwook Choi <flow@hrmm.xyz>
2024-04-17 22:38:19 +0900

    use synchronous timeout for Interpreter

--- src/main/scala/esmeta/interpreter/Interpreter.scala
+++ src/main/scala/esmeta/interpreter/Interpreter.scala
@@ -37,18 +37,17 @@ class Interpreter(
 ) {
   import Interpreter.*
 
+  lazy val startTime: Long = System.currentTimeMillis
+
   /** final state */
-  lazy val result: State = timeout(
-    {
-      while (step) {}
-      if (log)
-        pw.println(st)
-        pw.close
-        println("[Interpreter] Logging finished")
-      st
-    },
-    timeLimit,
-  )
+  lazy val result: State = {
+    while (step) {}
+    if (log)
+      pw.println(st)
+      pw.close
+      println("[Interpreter] Logging finished")
+    st
+  }
 
   /** ECMAScript parser */
   lazy val esParser: ESParser = cfg.esParser
@@ -64,7 +63,12 @@ class Interpreter(
 
       // garbage collection
       iter += 1
-      if (!detail && iter % 100_000 == 0) GC(st)
+      if (!detail && iter % 100_000 == 0) {
+        for (limit <- timeLimit)
+          val duration = System.currentTimeMillis - startTime
+          if (duration / 1000 > limit) throw TimeoutException("interp")
+        GC(st)
+      }
 
       // cursor
       eval(st.context.cursor)
