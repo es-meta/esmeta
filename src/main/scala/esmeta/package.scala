1fca65dbbba97f8b6dfe05e68cd08959d71b7d3c
Jihyeok Park <jihyeok_park@korea.ac.kr>
2024-05-10 15:02:08 +0900

    Update version

--- src/main/scala/esmeta/package.scala
+++ src/main/scala/esmeta/package.scala
@@ -6,7 +6,7 @@ import esmeta.error.NoEnvVarError
 val LINE_SEP = System.getProperty("line.separator")
 
 /** base project directory root */
-val VERSION = "0.3.3"
+val VERSION = "0.4.0"
 
 /** base project directory root */
 val BASE_DIR =
