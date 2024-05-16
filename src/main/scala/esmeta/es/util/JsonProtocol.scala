5e7e3982a0dccf48fa1bdf70823b8f7bc8cb4578
Yongwook Choi <flow@hrmm.xyz>
2024-05-16 20:40:16 +0900

    impl NodeView/CondView dump

--- src/main/scala/esmeta/es/util/JsonProtocol.scala
+++ src/main/scala/esmeta/es/util/JsonProtocol.scala
@@ -30,19 +30,39 @@ class JsonProtocol(cfg: CFG) extends StateJsonProtocol(cfg) {
     }
 
   // syntax-sensitive views
-  given viewDecoder: Decoder[View] = optionDecoder
-  given viewEncoder: Encoder[View] = optionEncoder
-  given nodeViewDecoder: Decoder[NodeView] = deriveDecoder
-  given nodeViewEncoder: Encoder[NodeView] = deriveEncoder
+  // given viewDecoder: Decoder[View] = optionDecoder
+  given viewEncoder: Encoder[View] =
+    Encoder.instance {
+      case None => Json.Null
+      case Some((enclosing, feature, path)) =>
+        Json.obj(
+          "enclosing" -> Json.fromValues(
+            enclosing.map(f => f.func.name.asJson),
+          ),
+          "feature" -> feature.func.name.asJson,
+          "path" -> path.map(_.toString).asJson,
+        )
+    }
+  // given nodeViewDecoder: Decoder[NodeView] = deriveDecoder
+  given nodeViewEncoder: Encoder[NodeView] =
+    Encoder.instance(nv =>
+      Json.obj(
+        "node" -> Json.obj(
+          "inst" -> nv.node.toString.asJson,
+          "func" -> cfg.funcOf(nv.node).name.asJson,
+        ),
+        "view" -> viewEncoder(nv.view),
+      ),
+    )
   given condViewDecoder: Decoder[CondView] = deriveDecoder
   given condViewEncoder: Encoder[CondView] = deriveEncoder
   given funcViewDecoder: Decoder[FuncView] = deriveDecoder
   given funcViewEncoder: Encoder[FuncView] = deriveEncoder
 
   // meta-info for each view or features
-  given nodeViewInfoDecoder: Decoder[NodeViewInfo] = deriveDecoder
+  // given nodeViewInfoDecoder: Decoder[NodeViewInfo] = deriveDecoder
   given nodeViewInfoEncoder: Encoder[NodeViewInfo] = deriveEncoder
-  given condViewInfoDecoder: Decoder[CondViewInfo] = deriveDecoder
+  // given condViewInfoDecoder: Decoder[CondViewInfo] = deriveDecoder
   given condViewInfoEncoder: Encoder[CondViewInfo] = deriveEncoder
 
   // coverage constructor
