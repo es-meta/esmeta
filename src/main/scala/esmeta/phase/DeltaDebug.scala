2e29750ea9552f788d56a5e6045a5ef13929d88d
Yongwook Choi <flow@hrmm.xyz>
2024-05-19 23:23:29 +0900

    compare assertions on delta-debug for handling completely different program

--- src/main/scala/esmeta/phase/DeltaDebug.scala
+++ src/main/scala/esmeta/phase/DeltaDebug.scala
@@ -21,62 +21,82 @@ case object DeltaDebug extends Phase[CFG, String] {
 
   def apply(cfg: CFG, cmdConfig: CommandConfig, config: Config): String =
     val filename = getFirstFilename(cmdConfig, this.name)
-    val code = readFile(filename)
-    val finalState = Interpreter(cfg.init.from(code))
-    val minified = Minifier.minifySwc(code) match
+    val originalCode = readFile(filename)
+    val finalState = Interpreter(cfg.init.from(originalCode))
+    val minifiedCode = Minifier.minifySwc(originalCode) match
       case Failure(exception) => error(s"[delta-debug] $exception")
       case Success(minified)  => minified
     val injected =
       Injector
         .replaceBody(
           cfg,
-          code,
-          minified,
+          originalCode,
+          minifiedCode,
           defs = true,
           timeLimit = Some(1000),
           log = false,
           ignoreProperties = "\"name\"" :: Nil,
         )
-        .toString
-    val result = JSEngine.runGraal(injected, Some(1000)) match
+    val testCode = injected.toString
+    val assertions = injected.assertions
+    val result = JSEngine.runGraal(testCode, Some(1000)) match
       case Success(v) if v.isEmpty => error("[delta-debug] pass")
       case result @ _              => result.fold(_.toString, identity)
     DeltaDebugger(
       cfg,
       delta => {
         println(s"[delta-debug] trial\n${delta}")
+        // 1. check if it is valid program
         JSEngine.runGraal(delta, Some(1000)) match
           case Failure(_) => false
-          case _ =>
+          case _          =>
+            // 2. if it is valid, minify it
             Minifier.minifySwc(delta) match
-              case Failure(_) => false
+              case Failure(_)             => false
               case Success(minifiedDelta) =>
-                val injected = Injector
-                  .replaceBody(
+                // 3. get assertions (final state) from the delta program
+                val deltaAssertions = Injector
+                  .getTest(
                     cfg,
-                    code,
-                    minifiedDelta,
+                    delta,
                     defs = true,
-                    timeLimit = Some(1000),
                     log = false,
+                    timeLimit = Some(1000),
                     ignoreProperties = "\"name\"" :: Nil,
                   )
-                  .toString
-                val test = JSEngine
-                  .runGraal(injected, Some(1000))
-                  .fold(_.toString, identity)
-                  .toString
-                println(s"injected: ${injected}")
-                println(s"test: ${test}")
-                if (test != result)
-                  println(
-                    s"expect:\n${result}\nactual:\n${test}",
-                  )
-                test == result
-
+                  .assertions
+                // 4. if assertions between two program (original <-> delta), discard program.
+                if (assertions != deltaAssertions)
+                  println(s"= assertions\n$assertions\n")
+                  println(s"= dassertions\n$deltaAssertions\n")
+                  false
+                else {
+                  // 5. get assertion-injected program from delta program
+                  val code = Injector
+                    .replaceBody(
+                      cfg,
+                      originalCode,
+                      minifiedDelta,
+                      defs = true,
+                      timeLimit = Some(1000),
+                      log = false,
+                      ignoreProperties = "\"name\"" :: Nil,
+                    )
+                    .toString
+                  val test = JSEngine
+                    .runGraal(code, Some(1000))
+                    .fold(_.toString, identity)
+                    .toString
+                  // 6. run and check output from the injected code
+                  if (test != result)
+                    println(
+                      s"expect:\n${result}\nactual:\n${test}",
+                    )
+                  test == result
+                }
       },
       detail = true,
-    ).result(code)
+    ).result(originalCode)
 
   def defaultConfig: Config = Config()
   val options: List[PhaseOption[Config]] = List(
