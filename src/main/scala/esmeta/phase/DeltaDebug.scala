fb3c69ccd493f30fef95eff3b95d34322cd833de
Yongwook Choi <flow@hrmm.xyz>
2024-05-19 20:00:07 +0900

    fix delta-debug command equality logic

--- src/main/scala/esmeta/phase/DeltaDebug.scala
+++ src/main/scala/esmeta/phase/DeltaDebug.scala
@@ -12,6 +12,7 @@ import esmeta.injector.Injector
 import esmeta.js.JSEngine
 import esmeta.js.minifier.Minifier
 import esmeta.util.BaseUtils.error
+import esmeta.injector.ReturnInjector
 
 case object DeltaDebug extends Phase[CFG, String] {
   val name = "delta-debugger"
@@ -22,59 +23,60 @@ case object DeltaDebug extends Phase[CFG, String] {
     val filename = getFirstFilename(cmdConfig, this.name)
     val code = readFile(filename)
     val finalState = Interpreter(cfg.init.from(code))
-    Minifier.minifySwc(code) match
+    val minified = Minifier.minifySwc(code) match
       case Failure(exception) => error(s"[delta-debug] $exception")
-      case Success(minified) => {
-        val injected = Injector
-          .replaceBody(
-            cfg,
-            code,
-            minified,
-            defs = true,
-            timeLimit = Some(1000),
-            log = false,
-            ignoreProperties = "\"name\"" :: Nil,
-          )
-          .toString
-        JSEngine.runGraal(injected, Some(1000)) match
-          case Success(_) => error("[delta-debug] pass")
-          case Failure(exception) => {
-            val original = exception.toString
-            DeltaDebugger(
-              cfg,
-              delta => {
-                println(s"[delta-debug] trial\n${delta}")
-                JSEngine.runGraal(delta, Some(1000)) match
-                  case Failure(_) => false
-                  case _ =>
-                    Minifier.minifySwc(delta) match
-                      case Failure(_) => false
-                      case Success(minified) =>
-                        val injected = Injector
-                          .replaceBody(
-                            cfg,
-                            delta,
-                            minified,
-                            defs = true,
-                            timeLimit = Some(1000),
-                            log = false,
-                            ignoreProperties = "\"name\"" :: Nil,
-                          )
-                          .toString
-                        JSEngine.runGraal(injected, Some(1000)) match
-                          case Success(v) =>
-                            println(s"[delta-debug]: graal success")
-                            false
-                          case Failure(exception) =>
-                            println(
-                              s"expect:\n${original}\nactual:\n${exception.toString}",
-                            )
-                            exception.toString == original
-              },
-              detail = true,
-            ).result(code)
-          }
-      }
+      case Success(minified)  => minified
+    val injected =
+      Injector
+        .replaceBody(
+          cfg,
+          code,
+          minified,
+          defs = true,
+          timeLimit = Some(1000),
+          log = false,
+          ignoreProperties = "\"name\"" :: Nil,
+        )
+        .toString
+    val result = JSEngine.runGraal(injected, Some(1000)) match
+      case Success(v) if v.isEmpty => error("[delta-debug] pass")
+      case result @ _              => result.fold(_.toString, identity)
+    DeltaDebugger(
+      cfg,
+      delta => {
+        println(s"[delta-debug] trial\n${delta}")
+        JSEngine.runGraal(delta, Some(1000)) match
+          case Failure(_) => false
+          case _ =>
+            Minifier.minifySwc(delta) match
+              case Failure(_) => false
+              case Success(minifiedDelta) =>
+                val injected = Injector
+                  .replaceBody(
+                    cfg,
+                    code,
+                    minifiedDelta,
+                    defs = true,
+                    timeLimit = Some(1000),
+                    log = false,
+                    ignoreProperties = "\"name\"" :: Nil,
+                  )
+                  .toString
+                val test = JSEngine
+                  .runGraal(injected, Some(1000))
+                  .fold(_.toString, identity)
+                  .toString
+                println(s"injected: ${injected}")
+                println(s"test: ${test}")
+                if (test != result)
+                  println(
+                    s"expect:\n${result}\nactual:\n${test}",
+                  )
+                test == result
+
+      },
+      detail = true,
+    ).result(code)
 
   def defaultConfig: Config = Config()
   val options: List[PhaseOption[Config]] = List(
