73aaf7b07a43fb0140387d396a823e20766133da
Jihyeok Park <jihyeok_park@korea.ac.kr>
2024-07-22 16:36:21 +0900

    Replace `EClamp` with `__CLAMP__` aux function

--- src/main/scala/esmeta/ir/util/Stringifier.scala
+++ src/main/scala/esmeta/ir/util/Stringifier.scala
@@ -64,6 +64,7 @@ class Stringifier(detail: Boolean, location: Boolean) {
       case Clo          => "<CLO>:"
       case Cont         => "<CONT>:"
       case BuiltinClo   => "<BUILTIN-CLO>:"
+      case Aux          => "<AUX>:"
     })
 
   // function parameters
@@ -155,8 +156,6 @@ class Stringifier(detail: Boolean, location: Boolean) {
       case EVariadic(vop, exprs) =>
         given Rule[Iterable[Expr]] = iterableRule(sep = " ")
         app >> "(" >> vop >> " " >> exprs >> ")"
-      case EClamp(target, lower, upper) =>
-        app >> "(clamp " >> target >> " " >> lower >> " " >> upper >> ")"
       case EMathOp(mop, exprs) =>
         given Rule[Iterable[Expr]] = iterableRule(sep = " ")
         app >> "(" >> mop >> " " >> exprs >> ")"
