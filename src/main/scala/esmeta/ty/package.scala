8910989c6c3a2e0e4bdfe2c4ba824d443d65e4c7
Jihyeok Park <jihyeok_park@korea.ac.kr>
2024-09-11 11:19:31 +0900

    Remove unnecessary IdxRefine class

--- src/main/scala/esmeta/ty/package.scala
+++ src/main/scala/esmeta/ty/package.scala
@@ -21,9 +21,6 @@ type Refinements = Map[RefinementKind, Map[Local, ValueTy]]
 enum RefinementKind:
   case True, False, Normal, Abrupt
 
-/** indexed type refinements */
-case class IdxRefine(idx: Int, kind: RefinementKind, ty: ValueTy)
-
 // -----------------------------------------------------------------------------
 // helpers
 // -----------------------------------------------------------------------------
