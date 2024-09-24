e021df63b0fa827e5d3e2eeab9addf6f46b48f63
Minseok Choe <choems@outlook.com>
2024-09-25 00:03:11 +0900

    Add Renamer

--- src/main/scala/esmeta/peval/Renamer.scala
+++ src/main/scala/esmeta/peval/Renamer.scala
@@ -1,18 +1,35 @@
 package esmeta.peval
 
 import esmeta.cfg.CFG
-import esmeta.ir.{Name, Temp}
-
+import esmeta.ir.{Name, Temp, Var, Global, Local}
 import scala.collection.mutable.{Map as MMap}
 
-class Renamer private(
-  names: MMap[(String, Int, Int), String] ,
-  temps: MMap[(Int, Int, Int), Int]
+class Renamer private (
+  // names: MMap[(String, Int, Int), String] ,
+  temps: MMap[(Int, Int, Int), Int],
+  private var countCall: Int = 0,
+  private var countTemp: Int = 0,
+  private var dynamicAddr: Int = 0,
 ) {
 
-  def get(name : Name)(using CFG): Name = ???
-  def get(idx : Temp, ctx : PContext)(using CFG): Temp = 
-    Temp(temps.getOrElse((idx.idx, ???, ???), newTempCount))
+  def get(x: Var, ctx: PContext)(using CFG): Var = x match
+    case Global(name) => Global(name)
+    case l: Local     => get(l, ctx)
+
+  def get(l: Local, ctx: PContext)(using CFG): Local = l match
+    case name: Name => get(name, ctx)
+    case idx: Temp  => get(idx, ctx)
+
+  def get(name: Name, ctx: PContext)(using cfg: CFG): Name =
+    Name(s"${name.name}_${cfg.fnameMap(ctx.func.name).id}_${ctx.callCount}")
+
+  def get(idx: Temp, ctx: PContext)(using cfg: CFG): Temp =
+    val key = (idx.idx, cfg.fnameMap(ctx.func.name).id, ctx.callCount);
+    temps.get(key) match
+      case None =>
+        val i = newTempCount;
+        temps += key -> i; Temp(i)
+      case Some(v) => Temp(v)
 
   def newCallCount =
     countCall += 1;
@@ -22,12 +39,10 @@ class Renamer private(
     countTemp += 1;
     countTemp
 
-  private var countCall = 0;
-  private var countTemp = 0;
   // 변수_함수id_callcount
   // (temp_함수id_callcount) -> map
 }
 
 object Renamer {
-  def apply() = new Renamer(MMap.empty, MMap.empty);
+  def apply() = new Renamer(MMap.empty);
 }
