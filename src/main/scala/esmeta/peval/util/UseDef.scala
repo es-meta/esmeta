b14b18ad3821ec0fcc8a80f1f33fead36880154e
Minseok Choe <choems@outlook.com>
2024-09-25 00:23:14 +0900

    Run formatter

--- src/main/scala/esmeta/peval/util/UseDef.scala
+++ src/main/scala/esmeta/peval/util/UseDef.scala
@@ -16,17 +16,30 @@ class UseDef(func: Func) {
 
   lazy val cfg = CFGBuilder(Program(List(func)), log = false)
   lazy val cfgFunc = cfg.fnameMap(func.name)
-  lazy val definitions : Map[Local, Set[Definition]] = cfgFunc.nodes.flatMap((node) => {
-    node match
-      case Block(id, insts, _) => List.from[NormalInst](insts).zipWithIndex.map[Set[Definition]]{ case (inst, i) => inst match
-          case ILet(lhs, expr) => Set(Definition(lhs, id -> Some(i)))
-          case IAssign(lhs : Local, expr) => Set(Definition(lhs, id -> Some(i)))
-          case IPop(lhs, list, front) =>Set(Definition(lhs, id -> Some(i)))
-          case _ => Set.empty[Definition]
-      }.reduce[Set[Definition]](_ | _)
-      case Call(id, callInst, _) => Set(Definition(callInst.lhs, id -> None))
-      case _ : Branch => Set.empty
-  }).groupMap[Local, Definition]{ case Definition(local, _) => local }{ case x => x }
+  lazy val definitions: Map[Local, Set[Definition]] = cfgFunc.nodes
+    .flatMap((node) => {
+      node match
+        case Block(id, insts, _) =>
+          List
+            .from[NormalInst](insts)
+            .zipWithIndex
+            .map[Set[Definition]] {
+              case (inst, i) =>
+                inst match
+                  case ILet(lhs, expr) => Set(Definition(lhs, id -> Some(i)))
+                  case IAssign(lhs: Local, expr) =>
+                    Set(Definition(lhs, id -> Some(i)))
+                  case IPop(lhs, list, front) =>
+                    Set(Definition(lhs, id -> Some(i)))
+                  case _ => Set.empty[Definition]
+            }
+            .reduce[Set[Definition]](_ | _)
+        case Call(id, callInst, _) => Set(Definition(callInst.lhs, id -> None))
+        case _: Branch             => Set.empty
+    })
+    .groupMap[Local, Definition] { case Definition(local, _) => local } {
+      case x => x
+    }
 
   // private lazy val workingSet = Set(cfgFunc.entry.id)
   private lazy val map = Map.empty[Int, GenKill]
@@ -38,7 +51,6 @@ class UseDef(func: Func) {
     ???
   }
 
-
   extension (node: Node) {
 
     def gen = memoized[Node, Set[Definition]](cache = genMap, key = node) {
@@ -47,9 +59,10 @@ class UseDef(func: Func) {
 
     def kill = memoized[Node, Set[Definition]](cache = killMap, key = node) {
       node match
-        case Block(id, _, _)   => Set.empty
-        case Call(id, callInst, next) => Set(Definition(callInst.lhs, id -> None))
-        case Branch(_, _, _, _, _)    => Set.empty
+        case Block(id, _, _) => Set.empty
+        case Call(id, callInst, next) =>
+          Set(Definition(callInst.lhs, id -> None))
+        case Branch(_, _, _, _, _) => Set.empty
     }
 
     def in = ???
@@ -63,7 +76,6 @@ class UseDef(func: Func) {
 object UseDef {
 
   case class GenKill(var gen: Set[Local], var kill: Set[Local])
-
 
   // extension (expr: Expr) {
   //   object Hi {
@@ -84,26 +96,24 @@ object UseDef {
 
   extension (inst: Inst) {
     def gen = inst match
-      case IExpr(expr) => 
-      case ILet(lhs, expr) =>
-      case IAssign(ref, expr) =>
-      case IExpand(base, expr) =>
-      case IDelete(base, expr) =>
-      case IPush(elem, list, front) =>
-      case IPop(lhs, list, front) =>
-      case IReturn(expr) =>
-      case IAssert(expr) =>
-      case IPrint(expr) =>
-      case INop() =>
+      case IExpr(expr)                   =>
+      case ILet(lhs, expr)               =>
+      case IAssign(ref, expr)            =>
+      case IExpand(base, expr)           =>
+      case IDelete(base, expr)           =>
+      case IPush(elem, list, front)      =>
+      case IPop(lhs, list, front)        =>
+      case IReturn(expr)                 =>
+      case IAssert(expr)                 =>
+      case IPrint(expr)                  =>
+      case INop()                        =>
       case IIf(cond, thenInst, elseInst) =>
-      case IWhile(cond, body) =>
-      case ICall(lhs, fexpr, args) =>
+      case IWhile(cond, body)            =>
+      case ICall(lhs, fexpr, args)       =>
       case ISdoCall(lhs, base, op, args) =>
-      case ISeq(insts) =>
+      case ISeq(insts)                   =>
 
   }
-  
-  
 
   private def memoized[K, V](cache: MMap[K, V], key: K)(value: => V): V =
     cache.get(key) match
@@ -114,6 +124,5 @@ object UseDef {
   private def fix[A](f: A => A, x: A): A =
     val fx = f(x)
     if (fx == x) then fx else fix(f, fx)
-
 
 }
