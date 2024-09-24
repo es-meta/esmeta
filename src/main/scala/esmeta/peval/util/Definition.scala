515fe3b9cff852c7e75c98173b502039328fd710
Minseok Choe <choems@outlook.com>
2024-09-25 00:23:14 +0900

    Run formatter

--- src/main/scala/esmeta/peval/util/Definition.scala
+++ src/main/scala/esmeta/peval/util/Definition.scala
@@ -2,11 +2,8 @@ package esmeta.peval.util
 
 import esmeta.ir.Local
 
-/**
-    * 
-    *
-    * @param local
-    * @param nodeId
-    * @param nth
-    */
-  case class Definition(local: Local, at: (Int, Option[Int]))
+/** @param local
+  * @param nodeId
+  * @param nth
+  */
+case class Definition(local: Local, at: (Int, Option[Int]))
