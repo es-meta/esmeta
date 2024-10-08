e94f7462d5444caf8a32d7fcbed8ea5c25af5285
Minseok Choe <choems@outlook.com>
2024-10-09 03:48:51 +0900

    Edit: fix renamer not to use CFG

--- src/main/scala/esmeta/peval/util/Renamer.scala
+++ src/main/scala/esmeta/peval/util/Renamer.scala
@@ -1,10 +1,10 @@
 package esmeta.peval.util
 
-import esmeta.cfg.CFG
 import esmeta.ir.{Name, Temp, Var, Global, Local}
 import esmeta.state.{DynamicAddr}
 import scala.collection.mutable.{Map as MMap}
 import esmeta.peval.pstate.*
+import esmeta.util.BaseUtils.cached
 
 class Renamer private (
   // names: MMap[(String, Int, Int), String] ,
@@ -14,19 +14,26 @@ class Renamer private (
   private var dynamicAddr: Long = 0,
 ) {
 
-  def get(x: Var, ctx: PContext)(using CFG): Var = x match
+  /* to distinguish func names when renaming locals, WITHOUT USING CFG. */
+  val funcNameToInt =
+    var count = 0;
+    cached { _ =>
+      count += 1; count
+    }
+
+  def get(x: Var, ctx: PContext): Var = x match
     case Global(name) => Global(name)
     case l: Local     => get(l, ctx)
 
-  def get(l: Local, ctx: PContext)(using CFG): Local = l match
+  def get(l: Local, ctx: PContext): Local = l match
     case name: Name => get(name, ctx)
     case idx: Temp  => get(idx, ctx)
 
-  def get(name: Name, ctx: PContext)(using cfg: CFG): Name =
-    Name(s"${name.name}_${cfg.fnameMap(ctx.func.name).id}_${ctx.sensitivity}")
+  def get(name: Name, ctx: PContext): Name =
+    Name(s"${name.name}_${funcNameToInt(ctx.func.name)}_${ctx.sensitivity}")
 
-  def get(idx: Temp, ctx: PContext)(using cfg: CFG): Temp =
-    val key = (idx.idx, cfg.fnameMap(ctx.func.name).id, ctx.sensitivity);
+  def get(idx: Temp, ctx: PContext): Temp =
+    val key = (idx.idx, funcNameToInt(ctx.func.name), ctx.sensitivity);
     temps.get(key) match
       case None =>
         val i = newTempCount;
