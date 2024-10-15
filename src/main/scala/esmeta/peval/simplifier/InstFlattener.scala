d39b77bdaa14adfbc04c90e2a0d889a14ff166c1
Minseok Choe <choems@outlook.com>
2024-10-15 23:17:19 +0900

    minor: refactor

--- src/main/scala/esmeta/peval/simplifier/InstFlattener.scala
+++ src/main/scala/esmeta/peval/simplifier/InstFlattener.scala
@@ -3,21 +3,18 @@ package esmeta.peval.simplifier
 import esmeta.ir.*
 
 object InstFlattener:
-  def apply(inst: Inst): Inst =
-    val insts = (inst match
-      case IIf(cond, thenInst, elseInst) =>
-        List(IIf(cond, apply(thenInst), apply(elseInst)).passCmt(from = inst))
-      case IWhile(cond, body) =>
-        List(IWhile(cond, apply(body)).passCmt(from = inst))
-      case ISeq(insts) => flat(ISeq(insts.map(apply))).insts
-      case INop()      => List.empty[Inst]
-      case i           => List(i)
-    )
-    insts.size match
-      case 1 => insts.head
-      case _ => ISeq(insts)
+  def apply(inst: Inst): Inst = inst match
+    case IIf(cond, thenInst, elseInst) =>
+      IIf(cond, apply(thenInst), apply(elseInst)).passCmt(from = inst)
+    case IWhile(cond, body) => IWhile(cond, apply(body)).passCmt(from = inst)
+    case ISeq(insts) =>
+      flat(ISeq(insts.map(apply))).insts match
+        case head :: Nil => head
+        case rest        => ISeq(rest)
+    case INop() => ISeq(Nil)
+    case i      => i
 
-  def flat(iseq: ISeq): ISeq = ISeq(iseq.insts.map {
+  private def flat(iseq: ISeq): ISeq = ISeq(iseq.insts.map {
     case ISeq(insts) => insts.map(apply)
     case i           => List(apply(i))
   }.flatten)
