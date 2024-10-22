54a9df61a27f4b7fc00294ff67658cc20e8d0063
Minseok Choe <choems@outlook.com>
2024-10-23 00:02:36 +0900

    edit: package.scala - dirs

--- src/main/scala/esmeta/package.scala
+++ src/main/scala/esmeta/package.scala
@@ -22,8 +22,9 @@ val ANALYZE_LOG_DIR = s"$LOG_DIR/analyze"
 val INJECT_LOG_DIR = s"$LOG_DIR/inject"
 val EVAL_LOG_DIR = s"$LOG_DIR/eval"
 val TEST262TEST_LOG_DIR = s"$LOG_DIR/test262"
-val IRINTERP_LOG_DIR = s"$LOG_DIR/ir-interp"
 val PEVAL_LOG_DIR = s"$LOG_DIR/peval"
+val IREVAL_LOG_DIR = s"$LOG_DIR/ir-eval"
+val IRPEVAL_LOG_DIR = s"$LOG_DIR/ir-peval"
 
 /** stack trace depth */
 val STACK_TRACE_DEPTH = 15
