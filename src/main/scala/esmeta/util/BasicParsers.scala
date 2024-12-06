d7e58aec635604e7e776c6731c75c95607026d34
Minseok Choe <choems@outlook.com>
2024-12-07 02:47:14 +0900

    fix: basic parser

--- src/main/scala/esmeta/util/BasicParsers.scala
+++ src/main/scala/esmeta/util/BasicParsers.scala
@@ -36,7 +36,7 @@ trait BasicParsers extends JavaTokenParsers {
     parseBy(parser)(str)
 
   // string literal
-  lazy val string = ("\"[\u0000-\u000F]\"".r | stringLiteral) ^^ {
+  lazy val string = ("\"[\u0000-\u000F]\"".r ||| stringLiteral) ^^ {
     case s =>
       StringContext processEscapes s.substring(1, s.length - 1)
   }
