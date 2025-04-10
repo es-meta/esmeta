a556060cb4f89034a1c7a236b425fa765132dbb6
Minseok Choe <choems@outlook.com>
2025-04-10 15:56:49 +0900

    Remove unused

--- src/main/scala/esmeta/lang/util/LangTyFromTy.scala
+++
@@ -1,9 +0,0 @@
-package esmeta.lang.util
-
-import esmeta.lang.Type
-import esmeta.ty.Ty
-
-// TODO temporary solution - use ty instead.
-object LangTypeFromTy {
-  def from(str: String): Type = Type(Ty.from(str))
-}
