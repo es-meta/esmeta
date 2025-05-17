89b0181c248ea3c8ca91442386c95e13ed2ca081
Minseok Choe <choems@outlook.com>
2025-05-18 00:08:52 +0900

    fix: use serialized route

--- src/main/scala/esmeta/web/WebServer.scala
+++ src/main/scala/esmeta/web/WebServer.scala
@@ -2,16 +2,21 @@ package esmeta.web
 
 import esmeta.cfg.CFG
 import esmeta.web.routes.*
+import akka.Done
+import akka.actor.CoordinatedShutdown
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.HttpMethods.*
 import akka.http.scaladsl.model.*
+import akka.http.scaladsl.model.HttpMethods.*
 import akka.http.scaladsl.server.*
-import akka.http.scaladsl.server.Directives.{cors => _, *}
-import StatusCodes.*
-import ch.megard.akka.http.cors.scaladsl.CorsDirectives.*
-import ch.megard.akka.http.cors.scaladsl.settings.*
+import akka.http.scaladsl.server.Directives.*
+import akka.http.scaladsl.settings.ServerSettings
+import akka.pattern.after
+import akka.stream.OverflowStrategy
+import akka.stream.scaladsl.{Sink, Source}
+import scala.concurrent.{Future, Promise}
+import scala.concurrent.duration.*
 import scala.io.StdIn
 
 class WebServer(cfg: CFG, port: Int) {
@@ -24,17 +29,13 @@ class WebServer(cfg: CFG, port: Int) {
     val exceptionHandler = ExceptionHandler {
       case e: Throwable =>
         e.printStackTrace
-        complete(HttpResponse(InternalServerError, entity = e.toString))
+        complete(
+          HttpResponse(StatusCodes.InternalServerError, entity = e.toString),
+        )
     }
 
-    // cors settings
-    val settings = CorsSettings.defaultSettings
-      .withAllowCredentials(false)
-      .withMaxAge(None)
-      .withAllowedMethods(List(GET, POST, PUT, DELETE))
-
     // root router
-    val rootRoute = cors(settings) {
+    val rootRoute = cors() {
       handleExceptions(exceptionHandler)(
         concat(
           pathPrefix("spec")(SpecRoute(cfg)), // spec route
@@ -45,7 +46,47 @@ class WebServer(cfg: CFG, port: Int) {
         ),
       )
     }
-    val bindingFuture = Http().newServerAt(ESMETA_HOST, port).bind(rootRoute)
+
+    // timeout for serial route
+    val timeoutDuration = 30.seconds
+
+    // serial queue with timeout
+    val queue = Source
+      .queue[(RequestContext, Promise[RouteResult])](
+        1024,
+        OverflowStrategy.backpressure,
+      )
+      .mapAsync(1) {
+        case (ctx, promise) =>
+          val routeFut = rootRoute(ctx)
+
+          val timeoutFut = after(timeoutDuration) {
+            Future.failed(new RuntimeException("Request timed out"))
+          }
+
+          val result = Future.firstCompletedOf(Seq(routeFut, timeoutFut))
+          result.onComplete(promise.complete)
+          result
+      }
+      .to(Sink.ignore)
+      .run()
+
+    // serialized route which handles request in fifo
+    val serialRoute: Route = ctx => {
+      val promise = Promise[RouteResult]()
+      queue.offer((ctx, promise))
+      promise.future
+    }
+
+    val bindingFuture = Http().newServerAt(ESMETA_HOST, port).bind(serialRoute)
+
+    CoordinatedShutdown(system).addTask(
+      CoordinatedShutdown.PhaseBeforeServiceUnbind,
+      "log-shutdown-starting",
+    ) { () =>
+      println("coordinated shutdown triggered, port will be unbind...")
+      Future.successful(Done)
+    }
 
     println(s"Server now online at http://$ESMETA_HOST:$port")
     println("Press RETURN to stop...")
