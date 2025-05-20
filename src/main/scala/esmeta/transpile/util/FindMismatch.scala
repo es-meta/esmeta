08a1feb23b4d475c8f16278b1d111985b0aad614
Minseok Choe <choems@outlook.com>
2025-05-21 03:00:02 +0900

    feat: find lca of two ast

---
+++ src/main/scala/esmeta/transpile/util/FindMismatch.scala
@@ -0,0 +1,49 @@
+package esmeta.transpile.util
+
+import esmeta.es.*
+
+enum Difference:
+  case Identical
+  case Diff(left: Ast, right: Ast)
+
+  def toOption: Option[(Ast, Ast)] = this match
+    case Identical  => None
+    case Diff(l, r) => Some((l, r))
+
+extension (ast: Ast)
+
+  def diffRoot(other: Ast): Difference =
+    // this check is not fast - maybe use a hash or something?
+    if ast =:= other then Difference.Identical
+    else
+      (ast, other) match
+        case (_: Hole, _) | (_, _: Hole) => ???
+
+        case (l: Syntactic, r: Lexical) => Difference.Diff(l, r)
+        case (l: Lexical, r: Syntactic) => Difference.Diff(l, r)
+        case (l @ Lexical(n1, s1), r @ Lexical(n2, s2)) =>
+          val cond = n1 == n2 && s1 == s2
+          if cond then Difference.Identical
+          else Difference.Diff(l, r)
+        case (l @ Syntactic(n1, a1, i1, c1), r @ Syntactic(n2, a2, i2, c2)) =>
+          val cond =
+            n1 == n2 && a1 == a2 && i1 == i2 && c1.length == c2.length &&
+            (c1.map(_.isDefined).toList == c2.map(_.isDefined).toList)
+          if !cond then Difference.Diff(l, r)
+          else
+            val diffResult = (c1 zip c2)
+              .map {
+                case (Some(c1), Some(c2)) =>
+                  c1.diffRoot(c2) // search recursively
+                case (None, None) => Difference.Identical // no child => no diff
+                case _            => ??? // impossible, already checked
+              }
+              .filter(diff => diff != Difference.Identical)
+              .toList // 자식별로 재귀 탐색
+            diffResult match
+              case Nil         => Difference.Identical
+              case diff :: Nil => diff
+              case _           => Difference.Diff(l, r)
+          end if
+
+  inline def >/<(other: Ast): Difference = diffRoot(other)
