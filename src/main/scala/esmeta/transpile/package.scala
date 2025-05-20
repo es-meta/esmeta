e483733b33cfbe1febaa452e96e12156c2b83034
Minseok Choe <choems@outlook.com>
2025-05-21 03:00:02 +0900

    feat: find lca of two ast

--- src/main/scala/esmeta/transpile/package.scala
+++ src/main/scala/esmeta/transpile/package.scala
@@ -8,10 +8,15 @@ import esmeta.transpile.util.*
 
 object Transpiler:
   def apply(ast: Ast)(using CFG): Ast = {
+    println(s"Transpiling ${ast.code}")
     val transpiledAst = TranspileWalker(summon[CFG]).walk(ast)
     val stringified = transpiledAst.toString
     assert(ast =:= ast)
-    println("printing " ++ transpiledAst.toCode)
+    println("printing " ++ transpiledAst.code)
+    for ((a, b) <- (ast >/< transpiledAst).toOption) do
+      println(s"After transpileation, difference is:")
+      println(s"As Code       : `${a.code}` != `${b.code}`")
+      println(s"As Production : ${a.name} != ${b.name}")
     transpiledAst
   }
 
@@ -31,11 +36,28 @@ class TranspileWalker(cfg: CFG) extends Walker {
   lazy val esParser = cfg.esParser
 
   override def walk(ast: Syntactic): Syntactic = ast match {
+    case Syntactic("CoalesceExpression", _, _, Some(e1) +: Some(e2) +: _) =>
+      esParser("Expression")
+        .from(s"(${e1.code} == null ? undefined : ${e2.code})")
+        .assertSyntactic
+    // .walkChildren
+
     case Syntactic("ExpressionStatement", args, rhsIdx, children) =>
       esParser("StatementList")
-        .from(s"throw 42; ${ast.toCode}")
+        .from(s"throw 42; ${ast.code}")
         .assertSyntactic
+    // .walkChildren
 
     case _ => super.walk(ast)
   }
+
+  extension (ast: Syntactic)
+    def walkChildren: Syntactic =
+      val newChildren = ast.children.map(walkOpt(_, walk))
+      ast.copy(children = newChildren)
+}
+
+extension (ast: Ast) {
+  def code(using CFG): String =
+    ast.toString(grammar = Some(summon[CFG].grammar))
 }
