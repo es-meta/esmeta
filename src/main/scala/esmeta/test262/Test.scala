c48d8d678d1bed70f5ef0436471ac1011261b3da
Minseok Choe <choems@outlook.com>
2025-06-05 21:53:28 +0900

    fix: test262 frontmatter parser

--- src/main/scala/esmeta/test262/Test.scala
+++ src/main/scala/esmeta/test262/Test.scala
@@ -4,6 +4,8 @@ import esmeta.TEST262_TEST_DIR
 import esmeta.test262.util.*
 import esmeta.util.SystemUtils.*
 import scala.io.Source
+import io.circe.Json
+import io.circe.yaml.scalayaml.parser as yamlParser
 
 /** tests in Test262 */
 case class Test(
@@ -51,91 +53,29 @@ object Test {
     val lines =
       try source.getLines.toList
       finally source.close()
-    val test = lines.dropWhile((x) => !(x contains "/*---")) match {
+    val frontmatterLines = lines.dropWhile((x) => !(x contains "/*---")) match {
       case Nil       => Nil
       case _ :: rest => rest.takeWhile((x) => !(x contains "---*/"))
     }
-    val (negative, flags, includes, locales, features, _, _, es5) =
-      test.foldLeft(
-        (
-          Option.empty[String],
-          List[String](),
-          List[String](),
-          List[String](),
-          List[String](),
-          false,
-          false,
-          false,
-        ),
-      ) {
-        case (
-              (
-                negative,
-                flags,
-                includes,
-                locales,
-                features,
-                isNeg,
-                isInclude,
-                isES5,
-              ),
-              line,
-            ) => {
-          val isES5n = if (line contains "es5id:") true else isES5
-          val isNegn = if (line contains "negative:") true else isNeg
-          val negativen =
-            if ((line contains "type:") && isNeg) Some(line.split(' ').last)
-            else negative
-          val flagsn =
-            if (line contains "flags:")
-              line
-                .dropWhile(_ != '[')
-                .tail
-                .takeWhile(_ != ']')
-                .split(", ")
-                .toList
-            else flags
-          val (includesn, isIncluden) =
-            if (line contains "includes:") line.dropWhile(_ != '[') match {
-              case "" => (List(), true)
-              case s =>
-                (
-                  s.tail.takeWhile(_ != ']').split(",").toList.map(_.trim),
-                  isInclude,
-                )
-            }
-            else (includes, isInclude)
-          val includesn2 =
-            if (isInclude && (line contains ".js"))
-              includesn :+ (line.split(' ').last)
-            else includesn
-          val localesn =
-            if (line contains "locale:")
-              line
-                .dropWhile(_ != '[')
-                .tail
-                .takeWhile(_ != ']')
-                .split(", ")
-                .toList
-            else locales
-          val featuresn =
-            if (line contains "features:") line.dropWhile(_ != '[') match {
-              case "" => List()
-              case s => s.tail.takeWhile(_ != ']').split(",").map(_.trim).toList
-            }
-            else features
-          (
-            negativen,
-            flagsn,
-            includesn2,
-            localesn,
-            featuresn,
-            isNegn,
-            isIncluden,
-            isES5n,
-          )
-        }
-      }
+    val frontmatter = frontmatterLines.mkString("\n")
+    val yaml: Json =
+      if (frontmatter.isEmpty) then Json.Null
+      else yamlParser.parse(frontmatter).getOrElse(???)
+
+    val negative =
+      yaml.hcursor.downField("negative").get[String]("type").toOption
+    val es5 = yaml.hcursor.downField("es5id").succeeded
+    val flags = yaml.hcursor.get[List[String]]("flags").toOption.getOrElse(Nil)
+    val includes = yaml.hcursor
+      .get[List[String]]("includes")
+      .toOption
+      .getOrElse(Nil)
+      .map(_.trim)
+      .filter(_.nonEmpty)
+    val features =
+      yaml.hcursor.get[List[String]]("features").toOption.getOrElse(Nil)
+    val locales =
+      yaml.hcursor.get[List[String]]("locale").toOption.getOrElse(Nil)
     val newIncludes =
       if (flags contains "async") includes :+ "doneprintHandle.js" else includes
     Test(
