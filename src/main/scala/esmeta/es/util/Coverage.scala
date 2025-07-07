baaaa65211227b398d25ea648223aee5a00fcd15
d01c2 <rickykhj@korea.ac.kr>
2025-07-07 13:59:36 +0900

    inst: fuzzer instrumentation

--- src/main/scala/esmeta/es/util/Coverage.scala
+++ src/main/scala/esmeta/es/util/Coverage.scala
@@ -4,6 +4,7 @@ import esmeta.{LINE_SEP, TEST262TEST_LOG_DIR}
 import esmeta.cfg.*
 import esmeta.injector.*
 import esmeta.interpreter.*
+import esmeta.fuzzer.InstInfo
 import esmeta.ir.{Expr, EParse, EBool}
 import esmeta.ty.{*, given}
 import esmeta.es.*
@@ -38,6 +39,9 @@ case class Coverage(
 
   // meta-info of each script
   private var _minimalInfo: Map[String, ScriptInfo] = Map()
+
+  // covered condition metadata while fuzzing
+  private var coveredCondMetadata: Map[Cond, InstInfo] = Map()
 
   // mapping from nodes/conditions to scripts
   private var nodeViewMap: Map[Node, Map[View, Set[Script]]] = Map()
@@ -104,7 +108,11 @@ case class Coverage(
       Interp(initSt, tyCheck, kFs, cp, timeLimit, isTargetNode, isTargetBranch)
     interp.result; interp
 
-  def check(script: Script, interp: Interp): (State, Boolean, Boolean) = {
+  def check(
+    script: Script,
+    interp: Interp,
+    instInfo: Option[InstInfo] = None,
+  ): (State, Boolean, Boolean) = {
     val Script(code, _) = script
     val finalSt = interp.result
 
@@ -141,6 +149,7 @@ case class Coverage(
       getScripts(condView) match
         case None =>
           update(condView, nearest, script); updated = true; covered = true
+          for (info <- instInfo) coveredCondMetadata += condView.cond -> info
         case Some(scripts) =>
           if (all) {
             update(condView, nearest, script)
@@ -181,6 +190,7 @@ case class Coverage(
     withScripts = true,
     withScriptInfo = true,
     withTargetCondViews = true,
+    withReachableFuncs = true,
     withUnreachableFuncs = true,
     withMsg = withMsg,
   )
@@ -191,6 +201,7 @@ case class Coverage(
     withScripts: Boolean = false,
     withScriptInfo: Boolean = false,
     withTargetCondViews: Boolean = false,
+    withReachableFuncs: Boolean = false,
     withUnreachableFuncs: Boolean = false,
     withMsg: Boolean = false,
   ): Unit = {
@@ -223,6 +234,14 @@ case class Coverage(
       noSpace = false,
     )
     log("Dumped branch coverage")
+
+    dumpJson(
+      name = "covered condition metadata",
+      data = coveredCondMetadata.toList.sortBy(_._1.id),
+      filename = s"$baseDir/covered-condition-metadata.json",
+      noSpace = false,
+    )
+    log("Dumped covered condition metadata")
 
     if (withScripts)
       dumpDir[Script](
@@ -261,6 +280,17 @@ case class Coverage(
         noSpace = false,
       )
       log("dumped target conds")
+    if (withReachableFuncs)
+      dumpFile(
+        name = "reachable functions",
+        data = cfg.funcs
+          .filter(f => nodeViewMap.contains(f.entry))
+          .map(_.name)
+          .sorted
+          .mkString(LINE_SEP),
+        filename = s"$baseDir/reach-funcs",
+      )
+      log("dumped unreachable functions")
     if (withUnreachableFuncs)
       dumpFile(
         name = "unreachable functions",
