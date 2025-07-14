253ab5dc467489332228e05c6ff97f9e154b9ae8
Jihyeok Park <jihyeok_park@korea.ac.kr>
2025-07-15 00:50:35 +0900

    refactor: generalize edge case handling in stringifier

--- src/main/scala/esmeta/es/util/Stringifier.scala
+++ src/main/scala/esmeta/es/util/Stringifier.scala
@@ -40,9 +40,8 @@ class Stringifier(
         for (symbol <- nameMap(name).rhsVec(rhsIdx).symbols) symbol match
           case Terminal(term)                          => app >> term >> " "
           case Empty | NoLineTerminator | _: Lookahead =>
-          case Optional(sym) if sym.getNt.isEmpty      =>
           case _ =>
-            cs.headOption match
+            if (symbol.getNt.isDefined) cs.headOption match
               case Some(hd) => hd.map(aux); cs = cs.tail
               case _        => error(s"invalid AST: $origAst")
     aux(origAst)
