9e33652cda7df1d2066b9a4c07e39f2b81dd568a
Minseok Choe <choems@outlook.com>
2025-08-20 14:25:31 +0900

    wip 2

---
+++ partial/e1.js
@@ -0,0 +1,1 @@
+@@[e1 : CoalesceExpressionHead] ?? @@[e2 : BitwiseORExpression]
---
+++ partial/e2.js
@@ -0,0 +1,8 @@
+var _a;
+(_a = (@@[e1 : Literal]) ) !== null &&
+_a !== void 0 ?
+  _a :
+  ( @@[e2 : BitwiseORExpression] );
+
+
+  
