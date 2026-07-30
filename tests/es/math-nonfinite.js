9ed19be82df384d02383fe6029ee2c0ddc019d33
배상우 / 코드싸이 <81800589+CodePsy-2001@users.noreply.github.com>
2026-07-30 11:28:57 +0900

    fix math/number inconsistency (#348)
    Co-authored-by: Jihyeok Park <jihyeok_park@korea.ac.kr>

---
+++ tests/es/math-nonfinite.js
@@ -0,0 +1,11 @@
+var x = Math.exp(1000);
+var y = Math.expm1(1000);
+var z = Math.cosh(1000);
+var w = Math.sinh(1000);
+var p = Math.pow(2, Number.MAX_VALUE);
+var q = Math.pow.call(0, -0.1, -Number.MAX_VALUE);
+var r = Math.sinh(-1000);
+var s = Math.sqrt(-1);
+var t = Math.pow(-1, 0.5);
+var u = Math.log(-1);
+var v = Math.pow(-2, 4294967297);
