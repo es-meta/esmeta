8cbdc62eeaba9d2bf1a9cbeb07d195596ddefbef
Minseok Choe <choems@outlook.com>
2026-09-03 02:39:38 +0900

    fixup

--- src/main/scala/esmeta/spec/Dfn.scala
+++ src/main/scala/esmeta/spec/Dfn.scala
@@ -1,12 +1,9 @@
 package esmeta.spec
 
-/** glossary definition defined by `dfn` elements in ECMA-262 */
+/** glossary definition defined by `dfn` elements */
 case class Dfn(
-  name: String, // text of the `dfn` element (e.g., `Completion Record`)
-  variants: List[String] =
-    Nil, // `variants` attribute (e.g., `Completion Records`)
-  id: Option[String] = None, // `id` attribute if exists
-  clauseId: String = "", // id of the enclosing `emu-clause` element
+  name: String,
+  variants: List[String] = Nil,
 ) extends SpecElem {
 
   /** all forms of this glossary term (its name and variants) */
